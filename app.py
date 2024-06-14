bf356e30617eabc8418af6cc6e8378d73a6b15be
Chiefopoku <kwabenaopokujnr@gmail.com>
2024-06-14 00:34:50 +0000

    remove CORS

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 from dotenv import load_dotenv
-from flask_cors import CORS
 import os
 
 # Load environment variables
